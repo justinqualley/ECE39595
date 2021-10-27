e82f0984e10936bb8951af3bda626d0858c849a2
justinqualley <54243022+justinqualley@users.noreply.github.com>
2021-10-27 00:28:43 -0400

    finished

--- HW7CPPAssigned/Node.h
+++ HW7CPPAssigned/Node.h
@@ -4,19 +4,19 @@
 #include "Int.h"
 #include "Node.h"
 using namespace std;
-template<class T> 
+template<class T> //Why do I use class and not typename?
 class Node { 
 private:
    T data;
-   Node *left = nullptr;
+   Node *left = nullptr; //Dont need <T> here?
    Node *right = nullptr;
 public:
    Node( );
    Node(T val);
    virtual void print( ) const;
-   virtual Node<T> *insertNode(T n);
+   virtual Node<T> *insertNode(T n); //How do I read this?
 };
-template<class T>Node<T>::Node( ) { }
+template<class T>Node<T>::Node( ) { } //How do I read this?
 
 template<class T>Node<T>::Node(T val) {
    data = val;
@@ -26,13 +26,14 @@ template<class T>void Node<T>::print( ) const {
    if (this->left != nullptr) {
       this->left->print( );
    }
-   std::cout << data << " ";
+   cout << data << " ";
    if (this->right != nullptr) {
       this->right->print( );
    }
 }
 
 template<class T>Node<T>* Node<T>::insertNode(T n) {
+   cout << "inserting: " << n << endl;      //Why do I need to use this?
    if (this->data == n) return this;
    if (n < this->data) {
       if (left != nullptr) return left->insertNode(n); 
--- HW7CPPAssigned/main.cpp
+++ HW7CPPAssigned/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Int.h"
 #include "Node.h"
+#include "Float.h"
 
 int main(int argc, char** args) {
    const int values[ ] {5, 1, 7, 15};
@@ -12,12 +13,21 @@ int main(int argc, char** args) {
    rootInt->print( );
    std::cout << std::endl;
 
-/*
+   const double dvalues[] {5.1, 1.1, 7.1, 15.1};
    Node<double>* rootDouble = new Node<double>(10.0);
    for (int i = 0; i < 4; i++) {
-      rootDouble->insertNode((double) values[i]); 
+      rootDouble->insertNode((double) dvalues[i]); 
    }
-   rootInt->print( );
+   rootDouble->print( );
    std::cout << std::endl;
-*/
+
+   const float fvalues[] {5.1, 1.1, 7.1, 15.1};
+   Node<Float>* rootFloat = new Node<Float>(Float(10.0));
+   for (int i = 0; i < 4; i++) {
+      rootFloat->insertNode(Float(fvalues[i])); 
+   }
+
+   rootFloat->print( );
+   std::cout << std::endl;
+
 }
